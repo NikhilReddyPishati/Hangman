3e19255208656b0c8b777f45a73b4677fc9e767a
Nikhil Reddy Pishati <nikhilreddypishati8@gmail.com>
2026-08-19 23:17:11 +0530

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -29,7 +29,7 @@ hangman_art = {0:("   ",
                   "/ \\")} #These are stored as Dictionary 
 def display_man(wrong_guesses):
     for line in hangman_art[wrong_guesses]:
-        print(line) #it travels from first key-value pair to next value if guess is wrong
+        print(line) #It travels from first key-value pair to next value if guess is wrong
 
 def dispaly_hint(hint):
     print(" ".join(hint)) #it displays hint but not included 
