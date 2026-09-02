286fd5c53f840f7bfa3fca232dc032326aaf040a
Nikhil Reddy Pishati <nikhilreddypishati8@gmail.com>
2026-09-02 22:21:26 +0530

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -53,7 +53,7 @@ def main():
        # loop
         guess = input ("Enter any letter").lower().#It converts the input to Small letters
         if guess in choice:
-            for i in range(len(choice)):
+            for i in range(len(choice)):#This is the loop
                 if choice[i]== guess:
                     hint[i]=guess
                     continue
