a4613e1afb6a79c6ab19b4bd633f1b6d8cc1915d
Nikhil Reddy Pishati <nikhilreddypishati8@gmail.com>
2026-09-02 22:25:59 +0530

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -75,7 +75,7 @@ def main():
         elif wrong_guesses>=len(hangman_art)-1:
             display_man(wrong_guesses)
             dispaly_answer(choice)
-            print("YOU LOSE😭😭")#Displays the 
+            print("YOU LOSE😭😭")#Displays the lose text
             is_running=False
 
             
