cb60371b10f7896af7f48e7bf8d10e67aa0e86ec
Nikhil Reddy Pishati <nikhilreddypishati8@gmail.com>
2026-09-02 22:25:24 +0530

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -70,7 +70,7 @@ def main():
 
         if "_" not in hint:
             print(f"you wrong guesses are :{wrong_guesses}")
-            print("YOU WIN!🏆🏆")
+            print("YOU WIN!🏆🏆")#Displays You
             is_running=False
         elif wrong_guesses>=len(hangman_art)-1:
             display_man(wrong_guesses)
